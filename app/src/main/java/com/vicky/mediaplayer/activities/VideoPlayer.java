662aa29e2ce85d1512682ffdf9da83f5400bf0d2
Lakshminarayanan <lakshmi.narayanan@gopassport.com>
2018-10-26 03:27:47 +0530

    successfully added fullscreen for video

--- app/src/main/java/com/vicky/mediaplayer/activities/VideoPlayer.java
+++ app/src/main/java/com/vicky/mediaplayer/activities/VideoPlayer.java
@@ -1,8 +1,11 @@
 package com.vicky.mediaplayer.activities;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -11,11 +14,23 @@ import com.vicky.mediaplayer.R;
 public class VideoPlayer extends AppCompatActivity {
 
     String uri;
+    Button btn_fullscreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+        btn_fullscreen = (Button) findViewById(R.id.btn_fullscreen);
+        btn_fullscreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // todo go to fullscreen view
+                Intent i = new Intent(VideoPlayer.this, FullscreenActivity.class);
+                i.putExtra("uri", uri);
+                startActivity(i);
+            }
+        });
+
         uri = getIntent().getStringExtra("uri");
         startVideo(uri);
     }
